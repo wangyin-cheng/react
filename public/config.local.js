89f95fcaf46f7240199bb5989f8d6fe91bb05ea7
wangtu <2413823269@qq.com>
2022-03-02 09:01:53 +0800

    init test

--- public/config.local.js
+++
@@ -1,4 +0,0 @@
-window.LOCAL_CONFIG = {
-    API_HOME: 'http://rap2api.taobao.org/app/mock/298579',
-    API_UPLOAD: ''
-}
--- src/redux/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import reducer from './reducer';
-
-
-const stort = createStore(reducer)
-
-export default stort
